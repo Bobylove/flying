38608b58a646fcbeb1ff328c5cfec115f6cabd4b
Fred <delon.frederic.pro.job@gmail.com>
2016-10-24 16:56:17 +0200

    end of day ! try to see weather now

--- app.js
+++ app.js
@@ -1,31 +1,26 @@
 $(document).ready(function(){
 	"use stict"
 	var url = 'http://192.168.1.21:3000/places';
+	var meteo = 'http://api.openweathermap.org/data/2.5/weather?q=Toulouse,fr&appid=e05300d9bacf77c059ab39927fd4909d';
 	$('#getPass').click(function(){
-		$('#list').hide();
 		$.get(url, function(name){
 			for(var i= 0; i < url.length; i++) {
 				$('#list').append('<li>'+ name.places[i].nom + '</li>'); 
 				$('#pass').text(name.places[5].password);
 			};
 		});
-
-		$.ajax({
-			dataMeteo: "meteo toulouse",
-			url : ' http://openweathermap.org',
-			type : 'GET',
-			dataType : 'HTML'
-		
-			});
-			
-			success :(function(meteo){
-				$('#meteo').weatherfeed(['FRXX0055']);
-				console.log("ok");
-				$("#weather").click(.HTML(););
-									})
 	});
 
+	$('#weather').click(function(){
+		$.get(meteo, function(data){
+			for(var i= 0; i < meteo.length; i++) {
+				$('#infos').text(data);
+			};
+		});	
 
+
+
+	});
 });
 
 
